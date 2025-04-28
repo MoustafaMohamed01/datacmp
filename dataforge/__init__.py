ffdce1f3c32f56e479b851e1fd096ef78322cc6c
Moustafa Mohamed <161119254+MoustafaMohamed01@users.noreply.github.com>
2025-04-28 19:50:53 +0300

    __init__

--- dataforge/__init__.py
+++ dataforge/__init__.py
@@ -1,4 +1,4 @@
 from .detailed_info import get_detailed
 
 __version__ = "0.1.0"
-print(f"DataForge Library (Version {__version__}) loaded successfully!")
+print(f"dataforge Library (Version {__version__}) loaded successfully!")
