cc04196d6af9d2b39eb728ae4bf9079533c7534d
Moustafa Mohamed <161119254+MoustafaMohamed01@users.noreply.github.com>
2025-04-29 00:48:03 +0300

    Add files via upload

--- dataforge/__init__.py
+++ dataforge/__init__.py
@@ -1,4 +1,5 @@
 from .detailed_info import get_detailed
+from .column_cleaner import clean_column_names
 
 __version__ = "0.1.0"
-print(f"dataforge Library (Version {__version__}) loaded successfully!")
+print(f"DataForge Library (Version {__version__}) loaded successfully!")
