14547466ae2d4f7613ec33d5b9ca5418322bd1fb
Moustafa Mohamed <161119254+MoustafaMohamed01@users.noreply.github.com>
2025-04-30 06:06:09 +0300

    Add files via upload

--- dataforge/detailed_info.py
+++ dataforge/detailed_info.py
@@ -5,9 +5,7 @@ def get_detailed(df):
 
     num_rows, num_cols = df.shape
     data = [
-        ["Number of Rows", num_rows, "", "", ""],
-        ["Number of Columns", num_cols, "", "", ""],
-        ["Column Name", "Dtype", "Null", "Not Null", "Mean"],
+        ["Column Name", "Dtype", "Null", "Not Null", "Mean", "Unique Val"],
     ]
 
     null_counts = df.isnull().sum()
@@ -17,8 +15,15 @@ def get_detailed(df):
         dtype = df[col].dtype
         null = null_counts.get(col, 0)
         not_null = total_counts.get(col, 0)
-        mean_val = df[col].mean() if pd.api.types.is_numeric_dtype(dtype) else "-"
-        data.append([col, dtype, null, not_null, mean_val])
+        mean_val = round(df[col].mean(), 2) if pd.api.types.is_numeric_dtype(dtype) else "-"
+        unique_values = df[col].nunique()
+        data.append([col, dtype, null, not_null, mean_val, unique_values])
+    
+    info = [
+        ["Number of Rows", num_rows],
+        ["Number of Rows", num_cols]
+    ]
 
-    return tabulate(data, headers="firstrow", tablefmt="grid")
+    return tabulate(info, headers=["Info", "Values"], tablefmt="rounded_outline") + "\n\n" + tabulate(data, headers="firstrow", tablefmt="rounded_outline")
+
 
