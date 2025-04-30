8c2b8c33525415b4eabd612372d7b9e19332738f
Moustafa Mohamed <161119254+MoustafaMohamed01@users.noreply.github.com>
2025-05-01 02:59:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="dataforge",
+    name="datacmp",
     version="0.1.0",
     author="Moustafa Mohamed",
     author_email="moustafa.mh.mohamed@gmail.com",
     description="A lightweight library for exploring and cleaning datasets for ML workflows.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/MoustafaMohamed01/dataforge",
+    url="https://github.com/MoustafaMohamed01/datacmp",
     packages=find_packages(),
     install_requires=[
         "pandas>=1.0.0",
