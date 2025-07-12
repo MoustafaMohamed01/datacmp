76dce11f446f7bca12244aefdf92a8a3378df11f
Moustafa Mohamed <161119254+MoustafaMohamed01@users.noreply.github.com>
2025-07-12 23:32:00 +0300

    Update setup.py v2.0.0

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="datacmp",
-    version="0.1.0",
+    version="2.0.0",
     author="Moustafa Mohamed",
     author_email="moustafa.mh.mohamed@gmail.com",
-    description="A lightweight library for exploring and cleaning datasets for ML workflows.",
+    description="A powerful and configurable library for exploratory data analysis (EDA) and data cleaning for machine learning workflows.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/MoustafaMohamed01/datacmp",
     packages=find_packages(),
     install_requires=[
         "pandas>=1.0.0",
-        "tabulate>=0.8.0"
+        "tabulate>=0.8.0",
+        "PyYAML>=6.0"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
